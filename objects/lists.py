c1a869f51f4a1b68e35638ba0c5d88ba255097cd
dzifors <ggrzegrzolka11@gmail.com>
2023-12-21 21:02:18 +0100

    prepare logic for adding editions (pt. 1)

--- objects/lists.py
+++ objects/lists.py
@@ -23,13 +23,41 @@ async def get_editions():
     return res
 
 
-async def get_edition(id: int):
-    res = await db.fetch_one("SELECT * FROM editions WHERE id = :id", {"id": id})
+async def get_edition_count():
+    res = await db.fetch_val("SELECT COUNT(*) count FROM editions")
 
     return res
 
 
-async def get_list(id: int):
-    res = await db.fetch_all("SELECT * FROM lists WHERE edition = :id", {"id": id})
+async def get_edition(edition: int):
+    res = await db.fetch_one("SELECT * FROM editions WHERE id = :id", {"id": edition})
 
     return res
+
+
+async def create_edition(name: str):
+    await db.execute("INSERT INTO editions (name) VALUES (:name)", {"name": name})
+
+    return True
+
+
+async def get_list(edition: int):
+    res = await db.fetch_all("SELECT * FROM lists WHERE edition = :id", {"id": edition})
+
+    return res
+
+
+async def fill_list(edition: int, entry_list: list[list[[str]]]):
+    query = (
+        "INSERT INTO lists (first_name, last_name, affiliation, place, edition) VALUES "
+    )
+    query_inserts = []
+
+    for item in entry_list:
+        query_inserts.append(
+            f"('{item[1]}', '{item[2]}', '{item[3]}', {int(item[0][:1])}, {edition})"
+        )
+
+    query += ",".join(query_inserts)
+
+    return query
